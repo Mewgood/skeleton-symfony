2a16a507f8c12f63f646038dff07b61154240abc
Dev <dev@Madss-MacBook-Pro.local>
2022-08-16 22:28:54 +0200

    Initial Commit

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace :vendor_namespace;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
